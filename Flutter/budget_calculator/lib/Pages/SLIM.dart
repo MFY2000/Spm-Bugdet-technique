794497fb0ca3b0d929f0192d7eeae3e19de33d61
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-17 22:37:22 +0500

    MFY auto commit at 17/11/2021 22:37:17

--- Flutter/budget_calculator/lib/Pages/SLIM.dart
+++ Flutter/budget_calculator/lib/Pages/SLIM.dart
@@ -36,17 +36,17 @@ class _SLIMCalculatorState extends State<SLIMCalculator> {
         TextFeildCustom(
           width_: 0.75,
           taskInput: inputTextLOC,
-          inputLabel: "Enter the value of LOC",
+          inputLabel: "Enter the value of LOC", isValid: true,
         ),
         TextFeildCustom(
           width_: 0.75,
           taskInput: inputTextC,
-          inputLabel: "Enter the value of C (610 to 57314)",
+          inputLabel: "Enter the value of C (610 to 57314)", isValid: true,
         ),
         TextFeildCustom(
           width_: 0.75,
           taskInput: inputTextT,
-          inputLabel: "Enter the value of t",
+          inputLabel: "Enter the value of t", isValid: true,
         ),
         TextButton(
             onPressed: () => {calculateSlim()},
