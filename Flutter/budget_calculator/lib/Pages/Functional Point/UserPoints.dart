732dea4d65a0ef4111d7c3bb0c45974214d12a81
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-17 22:37:21 +0500

    MFY auto commit at 17/11/2021 22:37:17

---
+++ Flutter/budget_calculator/lib/Pages/Functional Point/UserPoints.dart
@@ -0,0 +1,86 @@
+// ignore_for_file: file_names
+
+import 'package:budget_calculator/Custom/TextFeildCustom.dart';
+import 'package:budget_calculator/Pages/Functional%20Point/Model/FunctionalModel.dart';
+import 'package:flutter/material.dart';
+
+class UserPoint extends StatefulWidget {
+  const UserPoint({Key? key}) : super(key: key);
+
+  @override
+  _UserPointState createState() => _UserPointState();
+}
+
+class _UserPointState extends State<UserPoint> {
+  var userInput = inputControllerFP;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 155,
+              child: TextFeildCustom(
+                width_: 0.75,
+                taskInput: userInput[0].control,
+                inputLabel: userInput[0].display,
+                isValid: userInput[0].isValid,
+              ),
+            ),
+            SizedBox(
+              width: 155,
+              child: TextFeildCustom(
+                width_: 0.75,
+                taskInput: userInput[1].control,
+                inputLabel: userInput[1].display,
+                isValid: userInput[1].isValid,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 155,
+              child: TextFeildCustom(
+                width_: 0.75,
+                taskInput: userInput[2].control,
+                inputLabel: userInput[2].display,
+                isValid: userInput[2].isValid,
+              ),
+            ),
+            SizedBox(
+              width: 155,
+              child: TextFeildCustom(
+                width_: 0.75,
+                taskInput: userInput[3].control,
+                inputLabel: userInput[3].display,
+                isValid: userInput[3].isValid,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(
+              width: 155,
+              child: TextFeildCustom(
+                width_: 0.75,
+                taskInput: userInput[4].control,
+                inputLabel: userInput[4].display,
+                isValid: userInput[4].isValid,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
