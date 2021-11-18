427c72c8a6a7f837aabf22c6109b17e120d7afed
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-18 21:59:30 +0500

    MFY auto commit at 18/11/2021 21:59:01

---
+++ Flutter/budget_calculator/lib/Pages/UseCasePoint/UserInput.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class UserInput extends StatefulWidget {
+  const UserInput({Key? key}) : super(key: key);
+
+  @override
+  _UserInputState createState() => _UserInputState();
+}
+
+class _UserInputState extends State<UserInput> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        
+      ],
+    );
+  }
+}
