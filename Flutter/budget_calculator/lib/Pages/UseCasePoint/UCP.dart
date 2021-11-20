45442141c7e32c99525788e60f432a4c19fb09be
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-20 14:02:25 +0500

    MFY auto commit at 20/11/2021 13:40:40

--- Flutter/budget_calculator/lib/Pages/UseCasePoint/UCP.dart
+++ Flutter/budget_calculator/lib/Pages/UseCasePoint/UCP.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names
 
+import 'package:budget_calculator/Custom/Contain.dart';
 import 'package:budget_calculator/Pages/UseCasePoint/UserInput.dart';
 import 'package:flutter/material.dart';
 
@@ -11,11 +12,11 @@ class UseCasePoint extends StatefulWidget {
 }
 
 class _UseCasePointState extends State<UseCasePoint> {
-
-  late String toReturn;
-  late double UUCW;
-  late double UAW;
-
+  late int onCalculate = 0;
+  late List<String> lstToReturn = ["0.0", "0.0"];
+  late String printAnswer = "";
+  late double UUCW = 0;
+  late double UAW = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -49,7 +50,8 @@ class _UseCasePointState extends State<UseCasePoint> {
                   UserInput(
                     lstWeight: [5, 10, 15],
                     noOfChild: 3,
-                    onchange: onChange
+                    onchange: onChange,
+                    index: 0,
                   ),
                   const Divider(
                     color: Colors.grey,
@@ -69,17 +71,118 @@ class _UseCasePointState extends State<UseCasePoint> {
                   UserInput(
                     lstWeight: [1, 2, 3],
                     noOfChild: 3,
-                    onchange: onChange
+                    onchange: onChange,
+                    index: 1,
                   ),
+                  TextButton(
+                      onPressed: calculate,
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.height * .0125),
+                        width: MediaQuery.of(context).size.width * .75,
+                        height: MediaQuery.of(context).size.height * .05,
+                        alignment: Alignment.center,
+                        child: const Text(
+                          "Calculate",
+                          style: TextStyle(fontSize: 15, color: Colors.white),
+                        ),
+                        decoration: BoxDecoration(
+                            color: Colors.blue[600],
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(15.0))),
+                      )),
+                  onCalculate == 0
+                      ? Container()
+                      : (onCalculate == 1
+                          ? const CircularProgressIndicator()
+                          : getResult()),
                 ])));
   }
-  onChange(String toReturn, int index, double answer){
-    toReturn = toReturn;
-    if(index == 0) {
-      UUCW = answer;
-    }else{
-      UAW = answer;
-    }
 
+  onChange(String toReturn, int index, double answer) {
+    index == 0 ? UUCW = answer : UAW = answer;
+
+    lstToReturn[index] = "${index == 0 ? "UUCW" : "UAW"} = $toReturn";
+    lstToReturn[index] += "${index == 0 ? "UUCW" : "UAW"} = $answer";
+    lstToReturn[index] += "\n";
+
+  }
+
+   getResult() {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: double.infinity,
+          height: 400,
+          margin: const EdgeInsets.symmetric(vertical: 20),
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            border: Border.all(
+                color: const Color.fromARGB(255, 51, 204, 255), width: 1),
+            borderRadius: BorderRadius.circular(5),
+            shape: BoxShape.rectangle,
+          ),
+          child: Text(
+            printAnswer,
+            textAlign: TextAlign.left,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 15,
+            // ignore: prefer_const_constructors
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+        Positioned(
+            left: 50,
+            top: 12,
+            child: Container(
+              padding: EdgeInsets.only(bottom: 10),
+              color: Colors.white,
+              child: const Text(
+                'Solution',
+                style: TextStyle(color: Colors.black, fontSize: 16),
+              ),
+            )),
+      ],
+    );
+  }
+
+  calculate() {
+    setState(() {
+      onCalculate = 1;
+    });
+
+    if (validation()) {
+      double UUCP = UAW + UUCW;
+
+      printAnswer += lstToReturn[0];
+      printAnswer += "\n";
+
+      printAnswer += lstToReturn[1];
+      printAnswer += "\n";
+
+      printAnswer += "UUCP = UAW + UUCW";
+      printAnswer += "\n";
+      printAnswer += "UUCP = $UUCP";
+      setState(() {
+        onCalculate = 2;
+      });
+    } else {
+      setState(() {
+        onCalculate = 0;
+      });
+    }
+  }
+
+  bool validation() {
+    if (UUCW == 0.0) {
+      return false;
+    } else if (UUCW == 0.0) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
