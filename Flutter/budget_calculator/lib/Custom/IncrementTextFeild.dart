2313720a7c18e30cae48253fa413cc77c1b0244c
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-23 22:35:02 +0500

    MFY auto commit at 23/11/2021 21:55:47

--- Flutter/budget_calculator/lib/Custom/IncrementTextFeild.dart
+++ Flutter/budget_calculator/lib/Custom/IncrementTextFeild.dart
@@ -8,7 +8,11 @@ class NumericStepButton extends StatefulWidget {
 
   final ValueChanged<int> onChanged;
 
-  const NumericStepButton({Key? key, this.minValue = 0,required this.maxValue, required this.onChanged})
+  const NumericStepButton(
+      {Key? key,
+      this.minValue = 0,
+      required this.maxValue,
+      required this.onChanged})
       : super(key: key);
 
   @override
@@ -18,60 +22,66 @@ class NumericStepButton extends StatefulWidget {
 }
 
 class _NumericStepButtonState extends State<NumericStepButton> {
-
-  int counter= 0;
+  int counter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-            icon: Icon(
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        CircleAvatar(
+          radius: 15,
+          backgroundColor: Colors.blue,
+          child: IconButton(
+            icon: const Icon(
               Icons.remove,
-              color: Theme.of(context).accentColor,
+              color: Colors.white,
             ),
-            padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 18.0),
-            iconSize: 32.0,
-            color: Theme.of(context).primaryColor,
+            iconSize: 10.0,
             onPressed: () {
-              setState(() {
-                if (counter > widget.minValue) {
-                  counter--;
-                }
-                widget.onChanged(counter);
-              });
+              
+              onChangeState(counter > widget.minValue, true);
             },
           ),
-          Text(
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
             '$counter',
             textAlign: TextAlign.center,
-            style: TextStyle(
+            style: const TextStyle(
               color: Colors.black87,
               fontSize: 18.0,
               fontWeight: FontWeight.w500,
             ),
           ),
-          IconButton(
-            icon: Icon(
-              Icons.add,
-              color: Theme.of(context).accentColor,
+        ),
+        CircleAvatar(
+          radius: 15,
+          backgroundColor: Colors.blue,
+          child: Center(
+            child: IconButton(
+              icon: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+              iconSize: 10.0,
+              onPressed: () {
+                onChangeState(counter < widget.maxValue, false);
+              },
             ),
-            padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 18.0),
-            iconSize: 32.0,
-            color: Theme.of(context).primaryColor,
-            onPressed: () {
-              setState(() {
-                if (counter < widget.maxValue) {
-                  counter++;
-                }
-                widget.onChanged(counter);
-              });
-            },
           ),
-        ],
-      ),
+        ),
+      ],
     );
+  }
+
+  onChangeState(bool toChange, bool toGo) {
+    if (toChange) {
+      setState(() {
+        counter = toGo ? counter-1: counter+1;
+      });
+      widget.onChanged(counter);
+    }
   }
 }
