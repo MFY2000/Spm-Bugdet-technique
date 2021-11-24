7c6485e163156fe1a7223f34c5c4d1af89c8e646
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-11-24 15:42:39 +0500

    MFY auto commit at 24/11/2021 15:40:33

--- Flutter/budget_calculator/lib/Pages/Functional Point/ScaleFactor.dart
+++ Flutter/budget_calculator/lib/Pages/Functional Point/ScaleFactor.dart
@@ -2,24 +2,36 @@
 
 import 'package:budget_calculator/Custom/DropDown.dart';
 import 'package:budget_calculator/Custom/IncrementTextFeild.dart';
+import 'package:budget_calculator/Model/FunctionalModel.dart';
 import 'package:flutter/material.dart';
 
 class ScaleFactor extends StatefulWidget {
   final List<String> weightfactors;
+  final int limit;
 
-  const ScaleFactor({Key? key, required this.weightfactors}) : super(key: key);
+  const ScaleFactor(
+      {Key? key, required this.weightfactors, required this.limit})
+      : super(key: key);
 
   @override
   _ScaleFactorState createState() => _ScaleFactorState();
 }
 
 class _ScaleFactorState extends State<ScaleFactor> {
-  late Map<String, List<int>> multipleScale = {"weight": [], "Scale": []};
   late int noOfchild = 1;
+  late int limitLeft;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    limitLeft = widget.limit;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    print(multipleScale);
+    print("$limitLeft > $multipleScale");
     return Column(
       children: getNoChildren(),
     );
@@ -33,10 +45,13 @@ class _ScaleFactorState extends State<ScaleFactor> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           NumericStepButton(
-            maxValue: 14,
+            maxValue: limitLeft,
             minValue: 0,
             onChanged: (index) {
               multipleScale["weight"]![i] = index;
+              setState(() {
+                limitLeft = calculateScaleLeft(multipleScale["weight"]!);
+              });
             },
           ),
           DropDownLst(
@@ -45,19 +60,50 @@ class _ScaleFactorState extends State<ScaleFactor> {
                 multipleScale["Scale"]![i] =
                     widget.weightfactors.indexOf(index);
               }),
-          ElevatedButton(
-              onPressed: () {
-                if (!(multipleScale["Scale"]![i].isNaN ||
-                    multipleScale["weight"]![i].isNaN)) {
-                  setState(() {
-                    noOfchild++;
-                  });
-                }
-              },
-              child: const Text("Add"))
+          Row(
+            children: [
+              IconButton(
+                  onPressed: () {
+                    if (limitLeft != 0) {
+                      if (!(multipleScale["Scale"]![i] == 0 &&
+                          multipleScale["weight"]![i] == 0)) {
+                        setState(() {
+                          multipleScale["Scale"]!.add(0);
+                          multipleScale["weight"]!.add(0);
+
+                          limitLeft =
+                              calculateScaleLeft(multipleScale["weight"]!);
+                          noOfchild++;
+                        });
+                      }
+                    }
+                  },
+                  icon: const Icon(Icons.add)),
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      toRetun.removeAt(i);
+                      multipleScale["Scale"]!.removeAt(i);
+                      multipleScale["weight"]!.removeAt(i);
+                      limitLeft = calculateScaleLeft(multipleScale["weight"]!);
+                      noOfchild--;
+                    });
+                  },
+                  icon: const Icon(Icons.delete)),
+            ],
+          )
         ],
       ));
     }
     return toRetun;
+  }
+
+  int calculateScaleLeft(List<int> lst) {
+    int sum = 0;
+    for (var item in lst) {
+      sum += item;
+    }
+
+    return 14 - sum;
   }
 }
