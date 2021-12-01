26e1eeb0bb15451111e86be2286dc10925b3a502
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-01 19:05:10 +0500

    MFY auto commit at 01/12/2021 19:04:59

--- Flutter/budget_calculator/lib/main.dart
+++ Flutter/budget_calculator/lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        
       ),
       home: const MyHomePage(title: "Software Project Management"),
     );
