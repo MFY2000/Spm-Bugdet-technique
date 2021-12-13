85c084564f3c379dc97412e61cbd4655ff49c8f2
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-13 22:18:21 +0500

    MFY auto commit at 13/12/2021 22:18:18

---
+++ Flutter/budget_calculator/lib/Pages/Functional Point/UAF/UserWeightFactor.dart
@@ -0,0 +1,48 @@
+// ignore_for_file: file_names
+
+import 'package:budget_calculator/Model/FunctionalModel.dart';
+import 'package:budget_calculator/Pages/Functional%20Point/UAF/CardAdvance.dart';
+import 'package:flutter/material.dart';
+
+class UserWeightFactor extends StatefulWidget {
+  const UserWeightFactor({Key? key}) : super(key: key);
+
+  @override
+  _UserWeightFactorState createState() => _UserWeightFactorState();
+}
+
+class _UserWeightFactorState extends State<UserWeightFactor> {
+  late int noOfchild = 1;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: getDifferentCard());
+  }
+
+  getDifferentCard() {
+    List<Widget> toReturn = [];
+
+    List<dynamic> weight = [], limit = [];
+
+    for (var i = 0; i < 1; i++) {
+      weight.add([inputControllerFP[i].value]);
+      limit.add(inputControllerFP[i].value);
+      toReturn.add(CardAdvance(heading: inputStringFP[i], selection: i));
+    }
+
+    if (multipleWeight["Limit"] != limit) {
+      multipleWeight["weight"] = weight;
+      multipleWeight["Limit"] = limit;
+    }
+
+    print("from my weightFactor $multipleWeight");
+    return toReturn;
+  }
+}
