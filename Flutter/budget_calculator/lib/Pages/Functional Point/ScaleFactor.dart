6905691b6ff949b4ad968d712662027d8b4914da
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-13 22:18:22 +0500

    MFY auto commit at 13/12/2021 22:18:18

--- Flutter/budget_calculator/lib/Pages/Functional Point/ScaleFactor.dart
+++ Flutter/budget_calculator/lib/Pages/Functional Point/ScaleFactor.dart
@@ -6,103 +6,123 @@ import 'package:budget_calculator/Model/FunctionalModel.dart';
 import 'package:flutter/material.dart';
 
 class ScaleFactor extends StatefulWidget {
-  final List<String> weightfactors;
-  final int limit;
-
-  const ScaleFactor(
-      {Key? key, required this.weightfactors, required this.limit})
-      : super(key: key);
+  const ScaleFactor({Key? key}) : super(key: key);
 
   @override
   _ScaleFactorState createState() => _ScaleFactorState();
 }
 
 class _ScaleFactorState extends State<ScaleFactor> {
-  late int noOfchild = 1;
+  late int noOfchild = multipleScale["weight"].length;
   late int limitLeft;
-
   @override
   void initState() {
     // TODO: implement initState
 
-    limitLeft = widget.limit;
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: getNoChildren(),
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: Text(
+                  getValues(multipleScale["weight"], multipleScale["Scale"]),
+                  style: getStyle(),
+                )),
+            IconButton(onPressed: onAddChild, icon: const Icon(Icons.add)),
+          ],
+        ),
+        Column(
+          children: getNoChildren(),
+        )
+      ],
     );
+  }
+
+
+  onAddChild() {
+    if (getLimitLeft() != 0) {
+      if (!(multipleScale["Scale"].last == 0 &&
+          multipleScale["weight"].last == 0)) {
+        setState(() {
+          multipleScale["Scale"].add(0);
+          multipleScale["weight"].add(0);
+          noOfchild++;
+        });
+      }
+    }
   }
 
   getNoChildren() {
     List<Widget> toRetun = [];
 
-    for (var i = 0; i < noOfchild; i++) {
-      toRetun.add(Row(
+    for (var i = 1; i < noOfchild; i++) {
+      toRetun.add(
+        Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           NumericStepButton(
-            maxValue: limitLeft,
+            maxValue: (multipleScale["weight"]![i] + getLimitLeft()),
             minValue: 0,
             onChanged: (index) {
               multipleScale["weight"]![i] = index;
               setState(() {
-                limitLeft = calculateScaleLeft(multipleScale["weight"]!);
+                limitLeft = getLimitLeft();
               });
             },
           ),
           DropDownLst(
-              lstMethods: widget.weightfactors,
+              lstMethods: weightScale,//getLst(multipleScale["Scale"][i]),
               onSelect: (index) {
-                multipleScale["Scale"]![i] =
-                    widget.weightfactors.indexOf(index);
+                setState(() {
+                  multipleScale["Scale"][i] = weightScale.indexOf(index);
+                });
               }),
-          Row(
-            children: [
-              IconButton(
-                  onPressed: () {
-                    if (limitLeft != 0) {
-                      if (!(multipleScale["Scale"]![i] == 0 &&
-                          multipleScale["weight"]![i] == 0)) {
-                        setState(() {
-                          multipleScale["Scale"]!.add(0);
-                          multipleScale["weight"]!.add(0);
-
-                          limitLeft =
-                              calculateScaleLeft(multipleScale["weight"]!);
-                          noOfchild++;
-                        });
-                      }
-                    }
-                  },
-                  icon: const Icon(Icons.add)),
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      toRetun.removeAt(i);
-                      multipleScale["Scale"]!.removeAt(i);
-                      multipleScale["weight"]!.removeAt(i);
-                      limitLeft = calculateScaleLeft(multipleScale["weight"]!);
-                      noOfchild--;
-                    });
-                  },
-                  icon: const Icon(Icons.delete)),
-            ],
-          )
+              
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  // toRetun.removeAt(i);
+                  multipleScale["Scale"]!.removeAt(i);
+                  multipleScale["weight"]!.removeAt(i);
+                  limitLeft = getLimitLeft();
+                  noOfchild--;
+                }); 
+              },
+              icon: const Icon(Icons.delete)),
         ],
       ));
     }
     return toRetun;
   }
 
-  int calculateScaleLeft(List<int> lst) {
-    int sum = 0;
-    for (var item in lst) {
-      sum += item;
+  TextStyle getStyle() {
+    return const TextStyle(
+        color: Colors.black, fontWeight: FontWeight.w900);
+  }
+  String getValues(
+    List<dynamic> weight,
+    List<dynamic> scale,
+  ) {
+    String toReturn = "";
+    String temp = "";
+    int value;
+
+    for (var i = 0; i < weight.length; i++) {
+      value = scale[i] == 0 ? scale[0] : scale[i];
+      temp = weightScale[value];
+
+      if (weight[i] != 0) {
+        toReturn +=
+            "${i == 0 ? "All" : weight[i]} are ${temp.substring(0, 3)} (${--value}) ${i == 0 ? "except" : ","} ";
+      }
     }
 
-    return 14 - sum;
+    return toReturn;
   }
 }
