a8b5afc15e8e92993ed45e0bd2ad244d6a8000ac
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-15 22:17:59 +0500

    MFY auto commit at 15/12/2021 19:59:07

---
+++ Flutter/budget_calculator/lib/Custom/Heading/Heading1.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class Heading1 extends StatelessWidget {
+  final String heading;
+  final bool type;
+  const Heading1({Key? key, required this.heading,  required this.type}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 5),
+      alignment: Alignment.centerLeft,
+      child: Text(
+        heading,
+        style: TextStyle(
+            color: type ? Colors.teal : Colors.black54,
+            fontSize:type ? 21 : 18,
+            fontWeight: FontWeight.bold,
+            ),
+      ),
+    );
+  }
+}
