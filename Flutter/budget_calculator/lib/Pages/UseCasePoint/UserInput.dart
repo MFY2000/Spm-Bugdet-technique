5381b30d5b52ac38f4896b7e6ee00a8ff3506186
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-15 22:18:10 +0500

    MFY auto commit at 15/12/2021 19:59:07

--- Flutter/budget_calculator/lib/Pages/UseCasePoint/UserInput.dart
+++ Flutter/budget_calculator/lib/Pages/UseCasePoint/UserInput.dart
@@ -1,94 +1,121 @@
 // ignore_for_file: file_names
 
+import 'package:budget_calculator/Custom/Counter/Counter.dart';
+import 'package:budget_calculator/Custom/Heading/Heading1.dart';
 import 'package:budget_calculator/Custom/TextFeildCustom.dart';
 import 'package:budget_calculator/Model/UseCaseModel.dart';
 import 'package:flutter/material.dart';
 
 class UserInput extends StatefulWidget {
-  final int noOfChild;
-  final List<int> lstWeight;
-  final int index;
-  final Function(String toReturn, int index, double answer) onchange;
-  
-  UserInput({Key? key, required this.noOfChild, required this.lstWeight, required this.onchange, required this.index})
-      : super(key: key);
+  final String childName;
 
-  late List<int> userValueEnter = [0, 0, 0];
+  const UserInput({Key? key, required this.childName}) : super(key: key);
 
   @override
   _UserInputState createState() => _UserInputState();
 }
 
 class _UserInputState extends State<UserInput> {
-  double width = 0.0;
-  double answer = 0.0;
-  late List<UseCaseModel> lstInput = [];
+  double width = 0.0, height = 0.0;
+  String child = "UAW";
+  @override
+  void initState() {
+    // TODO: implement initState
+    child = widget.childName;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     width = (MediaQuery.of(context).size.width);
+    height = (MediaQuery.of(context).size.height);
 
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.end,
       children: [
+        Heading1(heading: child,type: false,),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: getTextFeild(),
         ),
         Container(
-          margin: EdgeInsets.symmetric(vertical: 10),
+          margin: const EdgeInsets.symmetric(vertical: 10),
           child: Text(
             ' = ${getAnswer()}',
             style: const TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.black),
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
           ),
+        ),
+        const Divider(
+          color: Colors.grey,
+          thickness: 2,
         ),
       ],
     );
   }
 
-  double getAnswer(){
-    double answer = 0;
-    String toReturn = "";
-    String temp= "";
-    for (var i = 0; i < widget.noOfChild; i++) {
-      temp = i < (widget.noOfChild - 1) ? "+": "";
-      toReturn += "(${widget.lstWeight[i]} * ${widget.userValueEnter[i]}) $temp";
-      answer += widget.lstWeight[i] * widget.userValueEnter[i]; 
+  getAnswer() {
+    num answer = 0;
+
+    List<dynamic> inputValue = input[child]["input"];
+    List<dynamic> inputwegiht = input[child]["weight"];
+
+    for (var i = 0; i < inputValue.length; i++) {
+      answer += inputValue[i] * inputwegiht[i];
     }
 
-    toReturn += "\n";
-    widget.onchange(toReturn, widget.index, answer);
+    input["UUCP"][child] = answer;
+
     return answer;
   }
 
   List<Widget> getTextFeild() {
     List<Widget> toReturnLst = [];
+    List<int> temp = [0, 0, 0];
+    bool isLimit = false;
 
-    for (int i = 0; i < widget.noOfChild; i++) {
-      lstInput.add(UseCaseModel(display: inputControllerUCP[i]));
+    for (int i = 0; i < input[child]["weight"].length; i++) {
+      isLimit = input[child]["limit"].length != 0;
+      temp[0] = isLimit ? input[child]["limit"][i] : 1000; // Limit (max)
+      temp[1] = isLimit && i != 0 ? input[child]["limit"][i - 1] : 0; // min
+      temp[2] = input[child]["weight"][i];
 
-      toReturnLst.add(SizedBox(
-        width: width * .25,
-        child: TextInput(
-            taskInput: lstInput[i].control,
-            inputLabel: lstInput[i].display,
-            isValid: lstInput[i].isValid,
-            onChange: onchangeValue,
-            index: i),
-      ));
+      toReturnLst.add(
+        SizedBox(
+          width: width * .23,
+          height: height * .075,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("${differentHeading[i]} (${temp[2]})"),
+              CounterButtons(
+                  count: temp[1],
+                  minValue: temp[1],
+                  maxValue: temp[0],
+                  onChanged: (value) {
+                    onChange(value, i);
+                  }),
+            ],
+          ),
+        ),
+      );
     }
 
     return toReturnLst;
   }
 
-  void onchangeValue(int index, String value) {
+  onChange(int value, int i) {
     setState(() {
-      widget.userValueEnter[index] = int.parse(value);
+      if ((input[child]["input"].length) < (i + 1)) {
+        for (var i = 0; i < 3; i++) {
+          input[child]["input"].add(0);
+        }
+        input[child]["input"][i] = value;
+      } else {
+        input[child]["input"][i] = value;
+      }
     });
   }
 }
-
