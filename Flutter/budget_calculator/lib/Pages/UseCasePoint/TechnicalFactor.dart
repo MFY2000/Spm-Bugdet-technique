a8ac5d34d2e41790056444f1ffbe8126a229c83c
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-15 22:18:03 +0500

    MFY auto commit at 15/12/2021 19:59:07

---
+++ Flutter/budget_calculator/lib/Pages/UseCasePoint/TechnicalFactor.dart
@@ -0,0 +1,133 @@
+// ignore_for_file: file_names
+
+import 'package:budget_calculator/Custom/Heading/Heading1.dart';
+import 'package:budget_calculator/Custom/Table/TableRowCard.dart';
+import 'package:budget_calculator/Model/UseCaseModel.dart';
+import 'package:flutter/material.dart';
+
+class TechnicalFactor extends StatefulWidget {
+  final bool typeFacorte;
+  const TechnicalFactor({Key? key, required this.typeFacorte})
+      : super(key: key);
+
+  @override
+  _TechnicalFactorState createState() => _TechnicalFactorState();
+}
+
+class _TechnicalFactorState extends State<TechnicalFactor> {
+  List<String> description = [];
+  String main = "TC";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    description = widget.typeFacorte ? descriptionFactor : descriptionEFactor;
+    main = widget.typeFacorte ? "TCF" : "EF";
+
+    for (var i = 0; i < description.length; i++) {
+      input[main]['input'].add(0);
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: getList());
+  }
+
+  getList() {
+    List<Widget> toReturn = [
+      getHeader(),
+    ];
+
+    for (var i = 0; i < description.length; i++) {
+      toReturn.add(TableRowCard(
+        data: [
+          description[i],
+          "${input["TC"]["weight"][i]}",
+          const [0, 5]
+        ],
+        onChange: (int value) {
+          onChange(i, value);
+        },
+      ));
+    }
+
+    toReturn.add(getTotal());
+    toReturn.add(getDivider());
+    return toReturn;
+  }
+
+  getHeader() {
+    return Column(
+      children: [
+        Heading1(
+          heading: main,
+          type: false,
+        ),
+        Row(children: [
+          Container(
+            decoration: BoxDecoration(border: Border.all()),
+            width: MediaQuery.of(context).size.width * .43,
+            height: 45,
+            alignment: Alignment.centerLeft,
+            child: const Text(
+              "Description",
+              style: TextStyle(fontWeight: FontWeight.w900),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all()),
+            width: MediaQuery.of(context).size.width * .14,
+            height: 45,
+            alignment: Alignment.center,
+            child: const Text(
+              "Weight",
+              style: TextStyle(fontWeight: FontWeight.w900),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(border: Border.all()),
+            width: MediaQuery.of(context).size.width * .23,
+            height: 45,
+            alignment: Alignment.center,
+            child: const Text(
+              "Rated Value\n(0 - 5)",
+              style: TextStyle(fontWeight: FontWeight.w900),
+            ),
+          ),
+        ]),
+      ],
+    );
+  }
+
+  onChange(int index, int value) {
+    setState(() {
+      input[main]['input'][index] = value;
+    });
+  }
+
+  getTotal() {
+    num total = 0;
+
+    for (var i = 0; i < input[main]['input'].length; i++) {
+      total += input[main]['input'][i] * input[main]['weight'][i];
+    }
+
+    return Container(
+      margin: const EdgeInsets.only(top: 5),
+      alignment: Alignment.centerRight,
+      child: Text("= $total",
+          style: const TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black)),
+    );
+  }
+
+  getDivider() {
+    return const Divider(
+      color: Colors.grey,
+      thickness: 2,
+    );
+  }
+}
