a172e0c18ae9a8cddc713e4ff8f42bb3b5d4b545
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-21 20:58:03 +0500

    MFY auto commit at 21/12/2021 20:56:38

--- Flutter/budget_calculator/lib/Model/UseCaseModel.dart
+++ Flutter/budget_calculator/lib/Model/UseCaseModel.dart
@@ -13,7 +13,7 @@ class UseCaseModel {
 List<String> differentHeading = ["Simple", "Average", "complex"];
 
 Map<String, dynamic> input = {
-  "UUCP": {"UUCW": 0, "UAW": 0},
+  "UUCP": {"UUCW": 0, "UAW": 0, "TC": 0, "EF": 0},
   "UUCW": {
     "input": [0,3,7],
     "weight": [5, 10, 15],
@@ -64,14 +64,23 @@ List<String> descriptionEFactor = [
 ];
 
 List<String> defualtStep = [
-  "UUCP = UUCW + UAW",
-  "UUCW = (5 * NSUC) + (10 * NAUC) + (15 * NCUC)",
-  "UAW = 1 * NSA + 2 * NAA + 3 * NCA",
-  "TFactor(Impact) = W * RV",
-  "TCF = 0.6 + (0.01 * TFactor)",
-  "1.4 + (-0.03 * EFactor)"
-  "UCP = UUCP * TCF * EF",
-  "UCP = ",
-  "UCP = ",
+  "UUCW = (5 * NSUC) + (10 * NAUC) + (15 * NCUC)", //0 1 2 3 4 5 6
+  "UUCW = 0", // 1
+  "UUCW = 0",// 2
+  "UAW = 1 * NSA + 2 * NAA + 3 * NCA",// 3
+  "UAW = 0",// 4
+  "UAW = 0",// 5
+  "UUCP = UUCW + UAW", // 6
+  "UUCP = ", // 7
+  "UUCP = ", // 8
+  "TCF = 0.6 + (0.01 * TFactor)", // 9
+  "TCF = 0.6 + (0.01 * ",// 10
+  "TCF = ",// 11
+  "EF = 1.4 + (-0.03 * EFactor)",// 12
+  "EF = 1.4 + (-0.03 * ",// 13
+  "EF = ",// 14
+  "UCP = UUCP * TCF * EF",// 15
+  "UCP = ",// 16
+  "UCP = ",// 17
 ];
 
