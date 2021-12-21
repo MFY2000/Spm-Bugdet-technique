2ab6799472d656d158264c9a2ba6c0eb10cb47d2
MFY2000 <fa19bsse0014@maju.edu.pk>
2021-12-21 20:58:02 +0500

    MFY auto commit at 21/12/2021 20:56:38

--- Flutter/budget_calculator/lib/Custom/Contain.dart
+++ Flutter/budget_calculator/lib/Custom/Contain.dart
@@ -1,13 +1,11 @@
 // ignore_for_file: file_names
 
-
 import 'package:flutter/material.dart';
-
 
 class Contain extends StatelessWidget {
   final String resultToString;
 
-  const Contain({ Key? key, required this.resultToString}) : super(key: key);
+  const Contain({Key? key, required this.resultToString}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,8 +16,8 @@ class Contain extends StatelessWidget {
           margin: const EdgeInsets.fromLTRB(20, 20, 20, 10),
           padding: const EdgeInsets.all(20),
           decoration: BoxDecoration(
-            border:
-                Border.all(color: const Color.fromARGB(255, 51, 204, 255), width: 1),
+            border: Border.all(
+                color: const Color.fromARGB(255, 51, 204, 255), width: 1),
             borderRadius: BorderRadius.circular(5),
             shape: BoxShape.rectangle,
           ),
